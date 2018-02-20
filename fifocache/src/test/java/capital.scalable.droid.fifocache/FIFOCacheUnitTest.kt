940dba28a66fd97443b7475ffeb58a230fd22e2b
Anas Ambri <anas.ambri@gmail.com>
2018-02-20 01:01:38 +0100

    Improve tests that use exceptions

--- fifocache/src/test/java/capital.scalable.droid.fifocache/FIFOCacheUnitTest.kt
+++ fifocache/src/test/java/capital.scalable.droid.fifocache/FIFOCacheUnitTest.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import java.io.InputStream
 
+
 @RunWith(MockitoJUnitRunner.StrictStubs::class)
 class FIFOCacheUnitTest {
+
+    @Rule @JvmField
+    val thrown = ExpectedException.none()
 
     companion object {
         private val TEST_FILE = "TEST_FILE"
@@ -44,41 +49,29 @@ class FIFOCacheUnitTest {
 
     @Test
     fun `set subdirectory to null throws an NullPointerException`() {
-        try {
-            cache.subdirectory = null
-            Assert.fail("setSubdirectory with null directory did not throw an exception")
-        } catch (e: NullPointerException) {
-            assertThat(e.message, `is`("Provided path was null"))
-        }
+        thrown.expect(NullPointerException::class.java)
+        thrown.expectMessage(`is`("Provided path was null"))
+        cache.subdirectory = null
     }
 
     @Test
     fun `set size to 0 throws an IllegalArgumentException`() {
-        try {
-            cache.size = 0
-            Assert.fail("set size to 0 did not throw an exception")
-        } catch (e: NullPointerException) {
-            assertThat(e.message, `is`("Cache size cannot be 0"))
-        }
+        thrown.expect(IllegalArgumentException::class.java)
+        thrown.expectMessage(`is`("Cache size cannot be 0"))
+        cache.size = 0
     }
 
     @Test
     fun `caching a file of negative size throws an IllegalArgumentException`() {
-        try {
-            cache.cache(inputStream, TEST_FILE, -1)
-            Assert.fail("Expected IllegalArgumentException")
-        } catch (e: IllegalArgumentException) {
-            assertThat(e.message, `is`("The provided stream size is smaller than 0"))
-        }
+        thrown.expect(IllegalArgumentException::class.java)
+        thrown.expectMessage(`is`("The provided stream size is smaller than 0"))
+        cache.cache(inputStream, TEST_FILE, -1)
     }
 
     @Test
     fun `caching a file larger than the cache size throws an IllegalArgumentException`() {
-        try {
-            cache.cache(inputStream, TEST_FILE, cache.size + 1)
-            Assert.fail("Expected IllegalArgumentException")
-        } catch (e: IllegalArgumentException) {
-            assertThat(e.message, `is`("The provided stream size is larger than the cache"))
-        }
+        thrown.expect(IllegalArgumentException::class.java)
+        thrown.expectMessage(`is`("The provided stream size is larger than the cache"))
+        cache.cache(inputStream, TEST_FILE, cache.size + 1)
     }
 }
